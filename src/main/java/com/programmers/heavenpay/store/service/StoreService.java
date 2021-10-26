330d14c13d2a3fc643a107ddb1d1e0aabe484868
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-26 22:15:39 +0900

    refactoer! service 반환형태 변경

--- src/main/java/com/programmers/heavenpay/store/service/StoreService.java
+++ src/main/java/com/programmers/heavenpay/store/service/StoreService.java
@@ -3,7 +3,10 @@ package com.programmers.heavenpay.store.service;
 import com.programmers.heavenpay.error.ErrorMessage;
 import com.programmers.heavenpay.error.exception.NotExistsException;
 import com.programmers.heavenpay.store.converter.StoreConverter;
+import com.programmers.heavenpay.store.dto.response.StoreCreateResponse;
+import com.programmers.heavenpay.store.dto.response.StoreDeleteResponse;
 import com.programmers.heavenpay.store.dto.response.StoreInfoResponse;
+import com.programmers.heavenpay.store.dto.response.StoreUpdateResponse;
 import com.programmers.heavenpay.store.entity.Store;
 import com.programmers.heavenpay.store.entity.vo.StoreType;
 import com.programmers.heavenpay.store.repository.StoreRepository;
@@ -20,22 +23,31 @@ public class StoreService {
     private final StoreConverter storeConverter;
 
     @Transactional
-    public Long create(String name, String typeStr, String vendorCode) {
+    public StoreCreateResponse create(String name, String typeStr, String vendorCode) {
         StoreType type = StoreType.of(typeStr);
 
         Store store = storeConverter.toStoreEntity(name, type, vendorCode);
         Store savedStore = storeRepository.save(store);
 
-        return savedStore.getId();
+        return StoreCreateResponse.builder()
+                .id(savedStore.getId())
+                .build();
     }
 
     @Transactional
-    public void update(Long id, String name, String typeStr, String vendorCode) {
+    public StoreUpdateResponse update(Long id, String name, String typeStr, String vendorCode) {
         Store store = storeRepository.findById(id)
                 .orElseThrow(() -> new NotExistsException(ErrorMessage.NOT_EXIST_STORE));
 
         StoreType type = StoreType.of(typeStr);
         store.changeInfo(name, type, vendorCode);
+
+        return StoreUpdateResponse.builder()
+                .id(id)
+                .name(name)
+                .type(typeStr)
+                .vendorCode(vendorCode)
+                .build();
     }
 
     @Transactional(readOnly = true)
@@ -53,10 +65,14 @@ public class StoreService {
     }
 
     @Transactional
-    public void delete(Long id) {
+    public StoreDeleteResponse delete(Long id) {
         Store store = storeRepository.findById(id)
                 .orElseThrow(() -> new NotExistsException(ErrorMessage.NOT_EXIST_STORE));
 
         storeRepository.delete(store);
+
+        return StoreDeleteResponse.builder()
+                .id(id)
+                .build();
     }
 }
