628401531bdd12b3181655402765cff87c0d9d92
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-27 14:29:16 +0900

    feat: 스토어 관련 응답 메시지 추가

--- src/main/java/com/programmers/heavenpay/common/dto/ResponseMessage.java
+++ src/main/java/com/programmers/heavenpay/common/dto/ResponseMessage.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ResponseMessage {
-    RESPONSE_EXAMPLE(HttpStatus.BAD_REQUEST, "예시 데이터입니다.");
+    STORE_INSERT_SUCCESS(HttpStatus.CREATED, "store 추가 성공"),
+    STORE_DELETE_SUCCCESS(HttpStatus.OK, "store 단건 삭제 성공"),
+    STORE_UPDATE_SUCCCESS(HttpStatus.OK, "store 수정 성공"),
+    STORE_SEARCH_SUCCCESS(HttpStatus.OK, "store 단건 조회 성공");
 
     private final HttpStatus status;
     private final String message;
