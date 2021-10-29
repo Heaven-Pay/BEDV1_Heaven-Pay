afeeea55980bb03d1f1b127790359bee08fc267c
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-29 17:51:56 +0900

    feat: produt converter 생성

---
+++ src/main/java/com/programmers/heavenpay/product/converter/ProductConverter.java
@@ -0,0 +1,28 @@
+package com.programmers.heavenpay.product.converter;
+
+import com.programmers.heavenpay.product.dto.response.ProductCreateResponse;
+import com.programmers.heavenpay.product.entitiy.Product;
+import com.programmers.heavenpay.product.entitiy.vo.Category;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ProductConverter {
+    public Product toProductEntity(String categoryStr, int price, String title, String description, int stock) {
+        return Product.builder()
+                .category(Category.of(categoryStr))
+                .description(description)
+                .price(price)
+                .stock(stock)
+                .title(title)
+                .build();
+    }
+
+    public ProductCreateResponse toProductCreateResponse(Long id, LocalDateTime createdDate, Object o) {
+        return ProductCreateResponse.builder()
+                .createdAt(createdDate)
+                .id(id)
+                .build();
+    }
+}
