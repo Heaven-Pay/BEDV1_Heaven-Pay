19889d3156806e85690e7659fe5d9ff6ba398996
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-29 19:03:38 +0900

    chore: 사용한 메소드 명시

--- src/main/java/com/programmers/heavenpay/product/repository/ProductRepository.java
+++ src/main/java/com/programmers/heavenpay/product/repository/ProductRepository.java
@@ -3,5 +3,12 @@ package com.programmers.heavenpay.product.repository;
 import com.programmers.heavenpay.product.entitiy.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Override
+    <S extends Product> S save(S entity);
+
+    @Override
+    Optional<Product> findById(Long aLong);
 }
