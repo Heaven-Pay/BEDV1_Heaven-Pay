6c6b598eacb75f6306005bdbdd0d5305e6aee5c1
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-29 17:55:23 +0900

    feat: 설정파일 path 추가

--- src/main/java/com/programmers/heavenpay/HeavenpayApplication.java
+++ src/main/java/com/programmers/heavenpay/HeavenpayApplication.java
@@ -2,10 +2,19 @@ package com.programmers.heavenpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class HeavenpayApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(HeavenpayApplication.class, args);
+        //SpringApplication.run(HeavenpayApplication.class, args);
+
+        new SpringApplicationBuilder(HeavenpayApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
