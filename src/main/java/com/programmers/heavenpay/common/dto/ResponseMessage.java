7f1c3d3a14275f3e6e0f6e78da6be5d3deced935
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-02 00:28:46 +0900

    response 메시지 추가

--- src/main/java/com/programmers/heavenpay/common/dto/ResponseMessage.java
+++ src/main/java/com/programmers/heavenpay/common/dto/ResponseMessage.java
@@ -14,7 +14,8 @@ public enum ResponseMessage {
     STORE_DELETE_SUCCESS(HttpStatus.OK, "store 단건 삭제 성공"),
     STORE_UPDATE_SUCCESS(HttpStatus.OK, "store 수정 성공"),
     STORE_SEARCH_SUCCESS(HttpStatus.OK, "store 조회 성공"),
-    PRODUCT_INSERT_SUCCESS(HttpStatus.CREATED, "product 생성 성공");
+    PRODUCT_INSERT_SUCCESS(HttpStatus.CREATED, "product 생성 성공"),
+    PRODUCT_SEARCH_SUCCESS(HttpStatus.OK, "product 단건 조회 성공");
 
     private final HttpStatus status;
     private final String message;
