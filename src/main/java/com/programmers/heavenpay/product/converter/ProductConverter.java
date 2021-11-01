407b8368f00af2ba652fc66a8e5672c46568fdcf
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-11-02 00:29:32 +0900

    feat: product converter 추가

--- src/main/java/com/programmers/heavenpay/product/converter/ProductConverter.java
+++ src/main/java/com/programmers/heavenpay/product/converter/ProductConverter.java
@@ -1,6 +1,7 @@
 package com.programmers.heavenpay.product.converter;
 
 import com.programmers.heavenpay.product.dto.response.ProductCreateResponse;
+import com.programmers.heavenpay.product.dto.response.ProductInfoResponse;
 import com.programmers.heavenpay.product.entitiy.Product;
 import com.programmers.heavenpay.product.entitiy.vo.Category;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,8 @@ import java.time.LocalDateTime;
 
 @Component
 public class ProductConverter {
+    private final static String EMPTY = "EMPTY";
+
     public Product toProductEntity(String categoryStr, int price, String title, String description, int stock) {
         return Product.builder()
                 .category(Category.of(categoryStr))
@@ -24,6 +27,22 @@ public class ProductConverter {
                 .createdAt(createdDate)
                 .s3Path(s3Path)
                 .id(id)
+                .build();
+    }
+
+    public ProductInfoResponse toProductInfoResponse(Product product) {
+        String s3Path = product.getS3Path().equals(EMPTY) ? EMPTY : product.getS3Path();
+
+        return ProductInfoResponse.builder()
+                .category(product.getCategory().getProductCategory())
+                .description(product.getDescription())
+                .id(product.getId())
+                .price(product.getPrice())
+                .s3Path(s3Path)
+                .score(product.getReviews().getScore())
+                .stock(product.getStock())
+                .storeId(product.getStore().getId())
+                .title(product.getTitle())
                 .build();
     }
 }
