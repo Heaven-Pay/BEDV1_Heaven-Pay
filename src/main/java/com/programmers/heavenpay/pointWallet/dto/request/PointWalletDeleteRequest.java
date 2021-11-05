a84746849141127a14989d96cb1736d585c68bef
UJHa <ddkk94@naver.com>
2021-11-05 14:22:27 +0900

    chore: 코드 리뷰 간 수정사항 적용

--- src/main/java/com/programmers/heavenpay/pointWallet/dto/request/PointWalletDeleteRequest.java
+++ src/main/java/com/programmers/heavenpay/pointWallet/dto/request/PointWalletDeleteRequest.java
@@ -6,12 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class PointWalletDeleteRequest {
+    @NotNull(message = "고객 아이디를 입력하세요.")
     @ArbitraryAuthenticationPrincipal
     private Long memberId;
 
+    @NotNull(message = "계좌정보를 입력하세요.")
     private Long accountId;
 }
